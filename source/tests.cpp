b3de0cff1ad79a5c3d17d8c799fde9c9bde8aff9
JaniTomati <jana.puschmann@uni-weimar.de>
2016-05-04 17:37:36 +0200

    News Alert: added new operators and tests (2.4)

--- source/tests.cpp
+++ source/tests.cpp
@@ -102,7 +102,7 @@ TEST_CASE("describe_operator_/=", "[Vec2]") {
 	Vec2 vc4 {0.0f, 0.0f};
 	Vec2 vc5 {-1.0f, -2.0f}; 
 
-	vc1 /= 2.0f;
+	vc1 /= 2.0f; 
 	REQUIRE(vc1.x == Approx(0.5f));
 	REQUIRE(vc1.y == Approx(1.0f));
 
@@ -122,6 +122,156 @@ TEST_CASE("describe_operator_/=", "[Vec2]") {
 	REQUIRE(vc5.x == Approx(-0.231213872f));
 	REQUIRE(vc5.y == Approx(-0.46242775f));
 
+	vc5 /= 0.0f;		// Fehlermeldung
+	REQUIRE(vc5.x == Approx(-0.231213872f));
+	REQUIRE(vc5.y == Approx(-0.46242775f));	
+}
+
+	// Test_Cases: w = u + v 
+TEST_CASE("describe_operator_addition", "[Vec2]") {
+	Vec2 vc1 {1.0f, 2.0f};
+	Vec2 vc2 {5.0f, 3.0f};
+	Vec2 vc3 {4.2f, 6.9f};
+	Vec2 vc4 {0.0f, 0.0f};
+	Vec2 vc5 {-1.0f, -2.0f}; 
+
+	Vec2 vc12 = vc1 + vc2; 
+	REQUIRE(vc12.x == 6.0f);
+	REQUIRE(vc12.y == 5.0f);
+
+	Vec2 vc13 = vc1 + vc3;
+	REQUIRE(vc13.x == 5.2f);
+	REQUIRE(vc13.y == 8.9f);
+
+	Vec2 vc23 = vc2 + vc3;
+	REQUIRE(vc23.x == 9.2f);
+	REQUIRE(vc23.y == 9.9f);
+
+	Vec2 vc35 = vc3 + vc5; 
+	REQUIRE(vc35.x == Approx(3.2f)); // test kept failing without Approx();
+	REQUIRE(vc35.y == 4.9f);
+
+	Vec2 vc45 = vc4 + vc5;
+	REQUIRE(vc45.x == -1.0f);
+	REQUIRE(vc45.y == -2.0f);
+}
+
+	// Test_Cases: w = u - v 
+TEST_CASE("describe_operator_subtraktion", "[Vec2]") {
+	Vec2 vc1 {1.0f, 2.0f};
+	Vec2 vc2 {5.0f, 3.0f};
+	Vec2 vc3 {4.2f, 6.9f};
+	Vec2 vc4 {0.0f, 0.0f};
+	Vec2 vc5 {-1.0f, -2.0f}; 
+
+	Vec2 vc12 = vc1 - vc2; 
+	REQUIRE(vc12.x == -4.0f);
+	REQUIRE(vc12.y == -1.0f);
+
+	Vec2 vc13 = vc1 - vc3; 
+	REQUIRE(vc13.x == Approx(-3.2f));
+	REQUIRE(vc13.y == -4.9f);
+
+	Vec2 vc23 = vc2 - vc3; 
+	REQUIRE(vc23.x == Approx(0.8f)); // test kept failing without Approx();
+	REQUIRE(vc23.y == -3.9f);
+
+	Vec2 vc35 = vc3 - vc5;
+	REQUIRE(vc35.x == 5.2f);
+	REQUIRE(vc35.y == 8.9f);
+
+	Vec2 vc45 = vc4 - vc5;
+	REQUIRE(vc45.x == 1.0f);
+	REQUIRE(vc45.y == 2.0f);
+}
+
+	// Test_Cases: u = v * s 
+TEST_CASE("describe_operator_multiplikation1", "[Vec2]") {
+	Vec2 vc1 {1.0f, 2.0f};
+	Vec2 vc2 {5.0f, 3.0f};
+	Vec2 vc3 {4.2f, 6.9f};
+	Vec2 vc4 {0.0f, 0.0f};
+	Vec2 vc5 {-1.0f, -2.0f}; 
+
+	Vec2 vcm1 = vc1 * 2.0f; 
+	REQUIRE(vcm1.x == 2.0f);
+	REQUIRE(vcm1.y == 4.0f);
+
+	Vec2 vcm2 = vc2 * 4.2f; 
+	REQUIRE(vcm2.x == 21.0f);
+	REQUIRE(vcm2.y == Approx(12.6f)); // test kept failing without Approx();
+
+	Vec2 vcm3 = vc3 * 6.9f;
+	REQUIRE(vcm3.x == 28.98f);
+	REQUIRE(vcm3.y == 47.61f);
+
+	Vec2 vcm4 = vc4 * 5.5f; 
+	REQUIRE(vcm4.x == 0.0f);
+	REQUIRE(vcm4.y == 0.0f);
+
+	Vec2 vcm5 = vc5 * -3.75;
+	REQUIRE(vcm5.x == 3.75f);
+	REQUIRE(vcm5.y == 7.5f);
+
+}
+
+	// Test_Cases: u = s * v 
+TEST_CASE("describe_operator_multiplikation2", "[Vec2]") {
+	Vec2 vc1 {1.0f, 2.0f};
+	Vec2 vc2 {5.0f, 3.0f};
+	Vec2 vc3 {4.2f, 6.9f};
+	Vec2 vc4 {0.0f, 0.0f};
+	Vec2 vc5 {-1.0f, -2.0f}; 
+
+	Vec2 vcm1 = 2.0f * vc1; 
+	REQUIRE(vcm1.x == 2.0f);
+	REQUIRE(vcm1.y == 4.0f);
+
+	Vec2 vcm2 = 4.2f * vc2; 
+	REQUIRE(vcm2.x == 21.0f);
+	REQUIRE(vcm2.y == Approx(12.6f)); // test kept failing without Approx();
+
+	Vec2 vcm3 = 6.9f * vc3;
+	REQUIRE(vcm3.x == 28.98f);
+	REQUIRE(vcm3.y == 47.61f);
+
+	Vec2 vcm4 = 5.5f * vc4; 
+	REQUIRE(vcm4.x == 0.0f);
+	REQUIRE(vcm4.y == 0.0f);
+
+	Vec2 vcm5 = -3.75 * vc5;
+	REQUIRE(vcm5.x == 3.75f);
+	REQUIRE(vcm5.y == 7.5f);
+
+}
+
+	// Test_Cases: u = v / s
+TEST_CASE("describe_operator_division", "[Vec2]") {
+	Vec2 vc1 {1.0f, 2.0f};
+	Vec2 vc2 {5.0f, 3.0f};
+	Vec2 vc3 {4.2f, 6.9f};
+	Vec2 vc4 {0.0f, 0.0f};
+	Vec2 vc5 {-1.0f, -2.0f}; 
+
+	Vec2 vcd1 = vc1 / 2.0f; 
+	REQUIRE(vcd1.x == 0.5f);
+	REQUIRE(vcd1.y == 1.0f);
+
+	Vec2 vcd2 = vc2 / 4.2f; 
+	REQUIRE(vcd2.x == Approx(1.190476));
+	REQUIRE(vcd2.y == Approx(0.714285));
+
+	Vec2 vcd3 = vc3 / 6.9f; 
+	REQUIRE(vcd3.x == Approx(0.608695f));
+	REQUIRE(vcd3.y == Approx(1.0f));
+
+	Vec2 vcd4 = vc4 / 0.0f;
+	REQUIRE(vcd4.x == Approx(0.0f));
+	REQUIRE(vcd4.y == Approx(0.0f));
+
+	Vec2 vcd5 = vc5 / -3.75;
+	REQUIRE(vcd5.x == Approx(0.266666f));
+	REQUIRE(vcd5.y == Approx(0.533333f));
 }
 
 
@@ -132,4 +282,5 @@ int main(int argc, char *argv[]) {
   return Catch::Session().run(argc, argv);
 
 }
+
 // * ----------------------------------- * //
--- source/vec2.cpp
+++ source/vec2.cpp
@@ -1,4 +1,4 @@
-// vec2.cpp
+// vec2.cpp (Programmiersprachen Aufgabenblatt 2)
 
 #include "vec2.hpp"
 #include <iostream>
@@ -13,31 +13,31 @@ x{x_}, y{y_} {
 
 } 
 
-// Operator += : Addiert zwei Vektoren und überschreibt den Vektor links vom Operator mit den neuen Werten
+// Operator += : Addiert zwei Vektoren und ueberschreibt den Vektor links vom Operator mit den neuen Werten
 Vec2& Vec2::operator += (Vec2 const& v) { 
 	x += v.x;
 	y += v.y;
 	return *this;
 }
 
-// Operator -= : Subtrahiert zwei Vektoren und überschreibt den Vektor links vom Operator mit den neuen Werten
+// Operator -= : Subtrahiert zwei Vektoren und ueberschreibt den Vektor links vom Operator mit den neuen Werten
 Vec2& Vec2::operator -= (Vec2 const& v) {
 	x -= v.x;
 	y -= v.y;
 	return *this;
 }
 
-// Operator *= : Multipliziert einen Vektor mit einer Gleitkommazahl und überschreibt den Vektor mit den neuen Werten
+// Operator *= : Multipliziert einen Vektor mit einer Gleitkommazahl und ueberschreibt den Vektor mit den neuen Werten
 Vec2& Vec2::operator *= (float v) {
 	x *= v;
 	y *= v; 
 	return *this;
 }
 
-// Operator *= : Dividiert eine Gleitkommazahl von einem Vektor und überschreibt den Vektor mit den neuen Werten
+// Operator /= : Dividiert eine Gleitkommazahl von einem Vektor und ueberschreibt den Vektor mit den neuen Werten
 Vec2& Vec2::operator /= (float v) {
 	if (v == 0.0f) {
-		std::cout << "/!\\ Die Teilung durch 0 ist nicht erlaubt!" << std::endl;
+		std::cout << "/!\\ Die Teilung durch 0 ist nicht erlaubt!" << std::endl; // Exception!
 	}
 
 	else {
@@ -46,4 +46,34 @@ Vec2& Vec2::operator /= (float v) {
 		return *this;
 	}
 
+}
+
+// Operator + : Addiert jeweils die X und Y-Werte zweier Vektoren und gibt einen neuen Vektor zurueck
+Vec2 operator + (Vec2 const& u, Vec2 const& v) {
+	return Vec2 {u.x + v.x, u.y + v.y};
+
+}
+
+// Operator - : Subtrahiert jeweils die X und Y-Werte zweier Vektoren und gibt einen neuen Vektor zurueck
+Vec2 operator - (Vec2 const& u, Vec2 const& v) {
+	return Vec2 {u.x - v.x, u.y - v.y};
+}
+
+// Operator * : Multipliziert jeweils die X und Y-Werte eines Vektors mit einer Gleitkommazahl und gibt einen neuen Vektor zurueck
+Vec2 operator * (Vec2 const& v, float s) {
+	return Vec2 {v.x * s, v.y * s};
+}
+
+Vec2 operator * (float s, Vec2 const& v) {
+	return Vec2 {s * v.x, s * v.y};
+}
+
+// Operator / : Dividiert jeweils die X und Y-Werte eines Vektors durch eine Gleitkommazahl und gibt einen neuen Vektor zurueck
+Vec2 operator / (Vec2 const& v, float s) { 
+	if (s == 0.0f) {
+		std::cout << "/!\\ Die Teilung durch 0 ist nicht erlaubt!" << std::endl;
+	}
+	else {
+		return Vec2 {v.x / s, v.y / s};
+	}
 }
